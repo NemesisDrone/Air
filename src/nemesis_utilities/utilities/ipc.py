ceb539aa5de87c5aed169cff1e158f3ca1ba8429
shaft <sh4ft.me@gmail.com>
2023-10-13 22:26:09 +0200

    feat(utilities): improve ipc module
    Renamed `Endpoint` to `Route`
    Switched from json to pickle
    Improved module doc
    Added blocking routes

--- src/nemesis_utilities/utilities/ipc.py
+++ src/nemesis_utilities/utilities/ipc.py
@@ -4,51 +4,63 @@ Overview & Usage
 This module implements a simple IPC system based on Redis.
 
 The :meth:`IpcNode <IpcNode>` class allow you to communicate with other nodes with
-an endpoint based system. An endpoint is a function decorated with
-:meth:`endpoint <endpoint>`, this function will be called when a message is
+a route based system. A route is a function decorated with
+:meth:`route <route>`, this function will be called when a message is
 received on the channel matching the regexes provided in the decorator.
 
 .. code-block:: python
 
     class PingPongNode(IpcNode):
 
-        # I register a ping endpoint, all messages sent to `ping` channel will be received by this function
-        @endpoint("ping")
-        def ping(self, payload: dict):
-            print("Ping!")
+    # I register a ping route, all messages sent to `ping` route will be received by this function
+    @route("ping")
+    def ping(self, payload: dict):
+        print("Ping!", flush=True)
 
-            # I send a message to the `pong` channel, since I want this node to receive it, I set `loopback` to True.
-            self.send("pong", {"extra_message": "Hello World!"}, loopback=True)
+        # I send a message to the `pong` route, since I want this node to receive it, I set `loopback` to True.
+        self.send("pong", {"extra_message": "Hello World!"}, loopback=True)
 
-        # I register a pong endpoint, all messages sent to `pong` channel will be received by this function
-        @endpoint("pong")
-        def pong(self, payload: dict):
-            print("Pong!")
-            # I can access the data sent by the ping function
-            print(payload["extra_message"])
+    # I register a pong route, all messages sent to `pong` route will be received by this function
+    @route("pong")
+    def pong(self, payload: dict):
+        print("Pong!", flush=True)
+        # I can access the data sent by the ping function
+        print(payload["extra_message"], flush=True)
+
+    # I register a blocking route, all messages sent to `return_pi` route will be received by this function
+    # Value will be sent back to the sender
+    @route("return_pi", blocking=True, thread=True)
+    def return_pi(self, payload: dict):
+        return 3.14159265359
 
 
     # I create a new node called `ipc-node-test`
     n = PingPongNode("ipc-node-test")
     # I start the node
     n.start()
-    # I send a message to the `ping` channel and use `loopback` since I want to receive it with the same node
+    # I send a message to the `ping` route and use `loopback` since I want to receive it with the same node
     n.send("ping", {}, loopback=True)
+    # I send a message to the `return_pi`  blocking route, this will wait for the response
+    print(n.send_blocking("return_pi", {}, loopback=True), flush=True)
+    # I don't forget to stop the node !
+    n.stop()
 
     '''
     Output:
     Ping!
     Pong!
     Hello World!
+    3.14159265359
     '''
 
 .. tip::
-    For more advanced usage, see the documentation of :meth:`endpoint <endpoint>`
+    For more advanced usage, see the documentation of :meth:`route <route>`
     decorator and :meth:`IpcNode <IpcNode>` class bellow.
 """
 import dataclasses
 import json
 import os
+import pickle
 import re
 import threading
 import time
@@ -56,14 +68,15 @@ import uuid
 
 import redis
 
+IPC_BLOCKING_RESPONSE_ROUTE = "IPC_BLOCKING_RESPONSE_ROUTE_IGNORE_IT"
 
-def endpoint(regex: str, *args, thread: bool = False):
+
+def route(regex: str, *args, thread: bool = False, blocking: bool = False):
     """
-    Decorate a method with this decorator to register it as an endpoint.
+    Decorate a method with this decorator to register it as a route.
 
     .. warning::
-        The decorated method must have a single parameter called `payload` of type :meth:`dict` to receive the
-        message extra data.
+        The decorated method must have a single parameter called `payload` to receive the data sent by the sender.
 
     :param regex: A regex to match the route. For example, if a message is sent to `a.b.c`, I can receive it by giving
         `a.*.c` as regex to this function, this will also work by giving `a.*`, `a.b.*`, `a.b.c` and more.
@@ -71,12 +84,46 @@ def endpoint(regex: str, *args, thread: bool = False):
     :param thread: If True, the decorated function will be called in a new thread. If this parameter is set to False,
         when the decorated function is called, the IPC node will not be able to receive new messages until the function
         returns.
+    :param blocking: If true, the decorated function return value will be sent back to the sender, the sender will
+        wait for the response before continuing.
+
+    .. warning::
+        With blocking mode, your function has to return None or a json serializable object only, otherwise a
+        warning will be printed and the function will return None.
+
+        Be also sure that your blocking route is registered ONLY ONCE. If you register it multiple times, the sender
+        will receive only the first response, but multiple callbacks will be called.
+    .. note::
+        If your function raises an exception, the exception will be automatically raised on the sender side.
     """
 
     def decorator(func):
-        func.regexes = [regex, *args]
-        func.thread = thread
-        return func
+
+        def wrapper(self, payload):
+            if blocking:
+                if IPC_BLOCKING_RESPONSE_ROUTE not in payload:
+                    print(f"IpcNode[{self.ipc_id}][{self.__class__.__name__}] ERROR, blocking route {regex} "
+                          f"received a request that was not sent with blocking mode, ignoring request", flush=True)
+                    return
+                else:
+                    blocking_route = payload.pop(IPC_BLOCKING_RESPONSE_ROUTE)
+
+                try:
+                    r = func(self, payload)
+                except Exception as e:
+                    r = e
+                try:
+                    self.send(blocking_route, r, loopback=True)
+                except Exception as e:
+                    print(f"IpcNode[{self.ipc_id}][{self.__class__.__name__}] ERROR, blocking route {regex} "
+                          f"failed to send response: {e}", flush=True)
+            else:
+                func(self, payload)
+
+        wrapper.regexes = [regex, *args]
+        wrapper.thread = thread
+
+        return wrapper
 
     return decorator
 
@@ -87,7 +134,7 @@ class IpcNode:
 
     .. note::
         This class is not meant to be used directly except if you just need to send messages,
-        you should inherit from it and implement your own endpoints.
+        you should inherit from it and implement your own routes.
     """
 
     def __init__(self, ipc_id: str = None, host: str = 'redis-ipc',
@@ -124,6 +171,9 @@ class IpcNode:
                 for regex in getattr(getattr(self, func), "regexes"):
                     self.regexes[regex] = (getattr(self, func), getattr(getattr(self, func), "thread"))
 
+        #: :meth: `dict` A dict mapping a blocking_response_route` to a semaphore and a response field in a list.
+        self.blocking_responses = {}
+
     def _listen(self):
         """
         Listener thread, this thread will listen for new messages and call the appropriate callbacks.
@@ -137,13 +187,8 @@ class IpcNode:
                 time.sleep(0.001)
                 continue
 
-            if message["channel"].decode() != "ipc":
-                continue
-
-            # print("MESSAGE RECEIVED", message, flush=True)
-
             try:
-                payload = json.loads(message["data"].decode())
+                payload = pickle.loads(message["data"])
                 if "sender" not in payload or "loopback" not in payload or "data" not in payload or "route" not in payload:
                     raise Exception("Malformed request")
             except Exception as e:
@@ -153,13 +198,19 @@ class IpcNode:
             if not payload["loopback"] and payload["sender"] == self.ipc_id:
                 continue
 
+            if payload["route"] in self.blocking_responses:
+                self.blocking_responses[payload["route"]][1] = pickle.loads(payload["data"])
+                self.blocking_responses[payload["route"]][0].release()
+                continue
+
             for regex in self.regexes:
                 if re.match(regex, payload["route"]) is not None:
                     try:
-                        self.regexes[regex][0](payload["data"]) if not self.regexes[regex][1] else threading.Thread(
-                            target=self.regexes[regex][0], args=(payload["data"],)).start()
+                        self.regexes[regex][0](pickle.loads(payload["data"])) if not self.regexes[regex][1] else threading.Thread(
+                            target=self.regexes[regex][0], args=(pickle.loads(payload["data"]),)).start()
                     except Exception as e:
-                        print(f"IpcNode[{self.ipc_id}] ERROR, callback raised exception {regex} {payload['data']}: {e}", flush=True)
+                        print(f"IpcNode[{self.ipc_id}] ERROR, callback raised exception {regex} {payload['data']}: {e}",
+                                flush=True)
 
         self.listening = False
 
@@ -188,39 +239,87 @@ class IpcNode:
         self.pubsub.close()
         self.r.close()
 
-    def send(self, route: str, data: dict, loopback: bool = False):
+    def send(self, route: str, data: object, loopback: bool = False):
         """
         Send a message to a route.
 
-        :param str route: The route that will be matched with regexes given in :meth:`endpoint <endpoint>` decorator.
-        :param dict data: Extra data to send as a dict, this data will be passed to the callback function.
+        :param str route: The route that will be matched with regexes given in :meth:`route <route>` decorator.
+        :param object data: Extra data to send as a dict, this data will be passed to the callback function. This data
+            must be pickle serializable.
         :param bool loopback: If True, this node will be able to receive the message. defaults to False.
         """
-        req = {"route": route, "sender": self.ipc_id, "loopback": loopback, "data": data}
-        self.r.publish("ipc", json.dumps(req).encode())
+        req = {"route": route, "sender": self.ipc_id, "loopback": loopback, "data": pickle.dumps(data)}
+        self.r.publish("ipc", pickle.dumps(req))
+
+    def send_blocking(self, route: str, data: dict, loopback: bool = False, timeout: float = 5.0):
+        """
+        Send a message to a blocking route and wait for the response / raise the exception.
+
+        :param str route: The route that will be matched with regexes given in :meth:`route <route>` decorator.
+
+        .. warning::
+            The given route has to match only one blocking route regex, see :meth:`route <route>` decorator.
+
+        :param dict data: Extra data to send as a dict, this data will be passed to the callback function.
+        :param bool loopback: If True, this node will be able to receive the message. defaults to False.
+        :param float timeout: Timeout in seconds, defaults to 5.0, will return None if timeout is reached.
+
+        :return: The response data of the blocking callback, None if this was returned by the callback or if timeout
+            is reached or if an error occurred in the callback return value serialization.
+
+        .. warning::
+            Always correctly check the return value of this function and catch the exception if any.
+
+        :raises: The exception raised by the callback if any.
+        """
+        blocking_route = f"ipc-blocking-{str(uuid.uuid4())}"
+        req = {"route": route, "sender": self.ipc_id, "loopback": loopback,
+               "data": pickle.dumps(data | {IPC_BLOCKING_RESPONSE_ROUTE: blocking_route})}
+        self.pubsub.subscribe(blocking_route)
+        self.blocking_responses[blocking_route] = [threading.Semaphore(0), None]
+        self.r.publish("ipc", pickle.dumps(req))
+        self.blocking_responses[blocking_route][0].acquire(timeout=timeout)
+        self.pubsub.unsubscribe(blocking_route)
+        r = self.blocking_responses[blocking_route][1]
+        del self.blocking_responses[blocking_route]
+        if isinstance(r, Exception):
+            raise r
+        else:
+            return r
+
 
 if __name__ == '__main__':
     class PingPongNode(IpcNode):
 
-        # I register a ping endpoint, all messages sent to `ping` channel will be received by this function
-        @endpoint("ping")
+        # I register a ping route, all messages sent to `ping` route will be received by this function
+        @route("ping")
         def ping(self, payload: dict):
-            print("Ping!")
+            print("Ping!", flush=True)
 
-            # I send a message to the `pong` channel, since I want this node to receive it, I set `loopback` to True.
+            # I send a message to the `pong` route, since I want this node to receive it, I set `loopback` to True.
             self.send("pong", {"extra_message": "Hello World!"}, loopback=True)
 
-        # I register a pong endpoint, all messages sent to `pong` channel will be received by this function
-        @endpoint("pong")
+        # I register a pong route, all messages sent to `pong` route will be received by this function
+        @route("pong")
         def pong(self, payload: dict):
-            print("Pong!")
+            print("Pong!", flush=True)
             # I can access the data sent by the ping function
-            print(payload["extra_message"])
+            print(payload["extra_message"], flush=True)
+
+        # I register a blocking route, all messages sent to `return_pi` route will be received by this function
+        # Value will be sent back to the sender
+        @route("return_pi", blocking=True, thread=True)
+        def return_pi(self, payload: dict):
+            return 3.14159265359
 
 
     # I create a new node called `ipc-node-test`
     n = PingPongNode("ipc-node-test")
     # I start the node
     n.start()
-    # I send a message to the `ping` channel and use `loopback` since I want to receive it with the same node
+    # I send a message to the `ping` route and use `loopback` since I want to receive it with the same node
     n.send("ping", {}, loopback=True)
+    # I send a message to the `return_pi`  blocking route, this will wait for the response
+    print(n.send_blocking("return_pi", {}, loopback=True), flush=True)
+    # I don't forget to stop the node !
+    n.stop()
