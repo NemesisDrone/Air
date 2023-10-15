9a1922984f39c047cc9afc8eb150c90f7e98cb1f
shaft <sh4ft.me@gmail.com>
2023-10-15 14:42:33 +0200

    docs(utilities): fix doc ident for ipc module

--- src/nemesis_utilities/utilities/ipc.py
+++ src/nemesis_utilities/utilities/ipc.py
@@ -12,26 +12,26 @@ received on the channel matching the regexes provided in the decorator.
 
     class PingPongNode(IpcNode):
 
-    # I register a ping route, all messages sent to `ping` route will be received by this function
-    @route("ping")
-    def ping(self, payload: dict):
-        self.log("Ping!")
+        # I register a ping route, all messages sent to `ping` route will be received by this function
+        @route("ping")
+        def ping(self, payload: dict):
+            self.log("Ping!")
+        
+            # I send a message to the `pong` route, since I want this node to receive it, I set `loopback` to True.
+            self.send("pong", {"extra_message": "Hello World!"}, loopback=True)
 
-        # I send a message to the `pong` route, since I want this node to receive it, I set `loopback` to True.
-        self.send("pong", {"extra_message": "Hello World!"}, loopback=True)
+        # I register a pong route, all messages sent to `pong` route will be received by this function
+        @route("pong")
+        def pong(self, payload: dict):
+            self.log("Pong!")
+            # I can access the data sent by the ping function
+            self.log(payload["extra_message"])
 
-    # I register a pong route, all messages sent to `pong` route will be received by this function
-    @route("pong")
-    def pong(self, payload: dict):
-        self.log("Pong!")
-        # I can access the data sent by the ping function
-        self.log(payload["extra_message"])
-
-    # I register a blocking route, all messages sent to `return_pi` route will be received by this function
-    # Value will be sent back to the sender
-    @route("return_pi", blocking=True, thread=True)
-    def return_pi(self, payload: dict):
-        return 3.14159265359
+        # I register a blocking route, all messages sent to `return_pi` route will be received by this function
+        # Value will be sent back to the sender
+        @route("return_pi", blocking=True, thread=True)
+        def return_pi(self, payload: dict):
+            return 3.14159265359
 
 
     # I create a new node called `ipc-node-test`
