9eea51579b8083f068d29b4abb378b7bf9039af8
shaft <sh4ft.me@gmail.com>
2023-12-09 22:33:36 +0100

    chore: update sync sh scripts

--- utils/pi-sync-shaft-rpi1.sh
+++ utils/pi-sync-shaft-rpi1.sh
@@ -1,4 +1,4 @@
 while true
 do
-    rsync -avz --inplace --delete --exclude-from='../rsync-ignore.txt' ../ nemesis@rpi1.dace-alpha.ts.net:"/home/nemesis/app"
+    sshpass -p 'nemesis' rsync -avz --inplace --delete --exclude-from='../rsync-ignore.txt' ../ nemesis@rpi1.dace-alpha.ts.net:"/home/nemesis/app"
 done
