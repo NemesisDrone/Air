90dd4be85e8dc97f4f4056839a479b4a1dc7343b
x <sh4ft.me@gmail.com>
2024-01-03 22:49:50 +0100

    feat: complete rework of manager and component
    Complete rework of manager and component to follow the ipc rework. Code quality and stability got increased.
    Testing of component and manager.

--- src/nemesis_utilities/utilities/component.py
+++ src/nemesis_utilities/utilities/component.py
@@ -1,237 +1,114 @@
-"""
-Overview & Usage
-----------------
-This module implements a component, a component represents and handle a microservice running in its own process.
-Components are managed by the manager and are meant to be subclassed. The subclass should override the :meth:`start`
-and :meth:`stop` methods and the :attr:`NAME` attribute.
-
-.. code-block:: python
-
-    class TestComponent(Component):
-        NAME = "test"
-
-        def __init__(self):
-            super().__init__()  # Very important, do not forget this line
-
-            # Do some init stuff here
-            self.log("test init")
-
-        def start(self):
-            self.log("test start")
-            time.sleep(1)
-
-        def stop(self):
-            self.log("test stop")
-            time.sleep(1)
-
-
-    test = TestComponent()
-    test.start()
-    time.sleep(2)
-    test.stop()
-
-    # Output without debug:
-    # [15-10-2023 12:35:56] INFO@test: test start
-    # [15-10-2023 12:35:59] INFO@test: test stop
-"""
-import dataclasses
 import time
+import traceback
 
-import utilities.ipc as ipc
+import redis
+from utilities import ipc, logger
 
 
-@dataclasses.dataclass
-class State:
-    """
-    Enumerations of all possible states of a component.
-    """
-
-    #: The component is starting up.
-    STARTING = "starting"
-    #: The component is running.
-    STARTED = "started"
-    #: The component is shutting down.
-    STOPPING = "stopping"
-    #: The component is not running.
+class ComponentState:
     STOPPED = "stopped"
+    STARTING = "starting"
+    STARTED = "started"
+    STOPPING = "stopping"
 
 
-class Component(ipc.IpcNode):
-    """
-    Represent a component. Components are managed by the manager and represents a single process.
+class Component:
+    NAME = None
 
-    .. note::
-        Components are not meant to be used directly, but rather to be subclassed. The subclass should override the
-        :meth:`start` and :meth:`stop` methods and the :attr:`NAME` attribute.
-    """
-
-    NAME = "component"
-
-    def __init__(self):
+    @staticmethod
+    def get_state(_redis: redis.Redis, component: str) -> str:
         """
-        Create a new component. The component will be in the :attr:`State.STOPPED` state.
+        Get the current state of the component
+        :param _redis: The redis instance to use
+        :param component: The component to get the state of
         """
-        super().__init__(ipc_id=self.__class__.NAME)
-        #: The current state of the component, picked from the :class:`State <State>` class.
-        self.state = State.STOPPED
+        return _redis.get(f"state:{component}").decode()
 
-        # Override start & stop methods
-        self.start = self._start_method(self.start)
-        self.stop = self._stop_method(self.stop)
-
-        # Override stop regex
-        self.regexes[f"^state:{self.NAME}:stop$"] = self.regexes.pop(
-            "^state:{NAME}:stop$"
-        )
-
-    @ipc.route("state:{NAME}:stop")
-    def _call_stop(self, payload: dict):
+    @staticmethod
+    def init_state(_redis: redis.Redis, component: str):
         """
-        Route to stop the component, should only be used by the manager.
+        Initialize the state of the component
+        :param _redis: The redis instance to use
+        :param component: The component to initialize the state of
         """
+        _redis.set(f"state:{component}", ComponentState.STOPPED)
+
+    def __init__(self, ipc_node: ipc.IpcNode):
+        assert self.__class__.NAME is not None
+
+        self._state = ComponentState.STOPPED
+
+        self._ipc_node = ipc_node
+
+        # Stop route
+        self._stop_component = ipc.Route([f"state:{self.NAME}:stop"], False).decorator(Component._stop_component)
+
+        self._ipc_node.bind_routes(self)
+        self._ipc_node.start()
+
+    @property
+    def logger(self) -> logger.Logger:
+        return self._ipc_node.logger
+
+    @property
+    def redis(self) -> redis.Redis:
+        return self._ipc_node.redis
+
+    @property
+    def ipc_node(self) -> ipc.IpcNode:
+        return self._ipc_node
+
+    def _update_state(self, state: str, from_state: str):
+        assert self._state == from_state
+        self._state = state
+
+        self._ipc_node.redis.set(f"state:{self.NAME}", state)
+        self._ipc_node.send(f"state:{self.NAME}:{state}", {"component": self.NAME})
+        self._ipc_node.logger.info(f"component is {state}", self.NAME, "state")
+
+    def _set_starting(self):
+        self._update_state(ComponentState.STARTING, ComponentState.STOPPED)
+
+    def _set_started(self):
+        self._update_state(ComponentState.STARTED, ComponentState.STARTING)
+
+    def _set_stopping(self):
+        self._update_state(ComponentState.STOPPING, ComponentState.STARTED)
+
+    def _set_stopped(self):
+        self._update_state(ComponentState.STOPPED, ComponentState.STOPPING)
+        self._ipc_node.stop()
+
+    def start_component(self):
+        self._set_starting()
+        self.start()
+        self._set_started()
+
+    def _stop_component(self, call_data, payload):
+        self._set_stopping()
         self.stop()
-
-    def _start_method(self, func):
-        def wrapper():
-            if self._set_starting():
-                r = func()
-                if not self._set_started():
-                    raise RuntimeError("Component failed to start.")
-                return r
-            else:
-                raise RuntimeError("Component is not stopped.")
-
-        return wrapper
-
-    def _stop_method(self, func):
-        def wrapper():
-            if self._set_stopping():
-                r = func()
-                if not self._set_stopped():
-                    raise RuntimeError("Component failed to stop.")
-                return r
-            else:
-                raise RuntimeError("Component is not started.")
-
-        return wrapper
-
-    def _set_starting(self) -> bool:
-        """
-        Low level method to set the component starting. Should be called by the :meth:`start` method when overriding.
-
-        :return: True if the component started successfully, False otherwise.
-        """
-        if self.state != State.STOPPED:
-            return False
-        super().start()
-        self.state = State.STARTING
-        self.send(
-            f"state:{self.__class__.NAME}:starting", {"component": self.__class__.NAME}
-        )
-        self.log(f"component starting", ipc.LogLevels.DEBUG, "state")
-        return True
-
-    def _set_started(self) -> bool:
-        """
-        Low level method to set the component started. Should be called by the :meth:`start` method when overriding.
-        """
-        if self.state != State.STARTING:
-            return False
-        self.state = State.STARTED
-        self.send(
-            f"state:{self.__class__.NAME}:started", {"component": self.__class__.NAME}
-        )
-        self.log(f"component started", ipc.LogLevels.DEBUG, "state")
-        return True
-
-    def _set_stopping(self) -> bool:
-        """
-        Low level method to set the component stopping. Should be called by the :meth:`stop` method when overriding.
-        """
-        if self.state != State.STARTED:
-            return False
-        self.state = State.STOPPING
-        self.send(
-            f"state:{self.__class__.NAME}:stopping", {"component": self.__class__.NAME}
-        )
-        self.log(f"component stopping", ipc.LogLevels.DEBUG, "state")
-        return True
-
-    def _set_stopped(self) -> bool:
-        """
-        Low level method to set the component stopped. Should be called by the :meth:`stop` method when overriding.
-        """
-        if self.state != State.STOPPING:
-            return False
-        self.state = State.STOPPED
-        self.send(
-            f"state:{self.__class__.NAME}:stopped", {"component": self.__class__.NAME}
-        )
-        self.log(f"component stopped", ipc.LogLevels.DEBUG, "state")
-        super().stop()
-        return True
+        self._set_stopped()
 
     def start(self):
-        """
-        Start the component. Should be overridden by the component.
-
-        .. danger::
-            This method should never send ipc messages to itself using the loopback parameter as this will cause a
-            deadlock.
-        """
-        pass
+        raise NotImplementedError()
 
     def stop(self):
-        """
-        Stop the component. Should be overridden by the component.
-
-        .. danger::
-            This method should never send ipc messages to itself using the loopback parameter as this will cause a
-            deadlock.
-        """
-        pass
-
-    # We reimplement this method, ignore the signature warning
-    # noinspection PyMethodOverriding
-    def log(
-        self, message: str, level: str = ipc.LogLevels.INFO, extra_route: str = None
-    ):
-        """
-        Log a message to stdout and to ipc system as "log.{level}.{component_name}" route.
-
-        :param str message: The message to log.
-        :param str level: The log level, pick it from :meth:`LogLevels <ipc.LogLevels>`, defaults to LogLevels.INFO.
-        :param str extra_route: An additional extra route that will be appended to the route, for example, if I give
-            `a.b.c` as extra route, the message will be sent to `log.{level}.{label}.a.b.c` route, defaults to "" (resulting in
-            `log.{level}.{component_name}` route).
-        """
-        super().log(message, level, self.__class__.NAME, extra_route)
+        raise NotImplementedError()
 
 
-if __name__ == "__main__":
+def run_component(component_type: Component):
+    strict_redis = redis.StrictRedis("redis")
+    ipc_node = ipc.IpcNode(
+        ipc_id=component_type.NAME,
+        strict_redis=strict_redis,
+        pubsub=strict_redis.pubsub(),
+    )
+    ipc_node.set_logger(logger.Logger(ipc_node))
 
-    class TestComponent(Component):
-        NAME = "test"
-
-        def __init__(self):
-            super().__init__()  # Very important, do not forget this line
-
-            # Do some init stuff here
-            self.log("test init")
-
-        def start(self):
-            self.log("test start")
-            time.sleep(1)
-
-        def stop(self):
-            self.log("test stop")
-            time.sleep(1)
-
-    test = TestComponent()
-    test.start()
-    time.sleep(2)
-    test.stop()
-
-    # Output without debug:
-    # [15-10-2023 12:35:56] INFO@test: test start
-    # [15-10-2023 12:35:59] INFO@test: test stop
+    try:
+        comp = component_type(ipc_node)
+        comp.start_component()
+    except Exception as e:
+        ipc_node.logger.error(f"Could not start component: {e}\n{traceback.format_exc()}", component_type.NAME)
+        ipc_node.stop()
+        return
---
+++ tests/nemesis_utilities/test_component.py
@@ -0,0 +1,248 @@
+import threading
+import unittest.mock
+
+import pytest
+from unittest.mock import Mock
+
+import redis
+from utilities import component as component_module, logger, ipc
+
+
+# --- Component States ---
+def test_component_states():
+    assert component_module.ComponentState.STOPPED == "stopped"
+    assert component_module.ComponentState.STARTING == "starting"
+    assert component_module.ComponentState.STARTED == "started"
+    assert component_module.ComponentState.STOPPING == "stopping"
+
+
+# --- Component ---
+def test_component_init_no_name():
+    mock_ipc_node = Mock()
+
+    with pytest.raises(AssertionError):
+        component_module.Component(mock_ipc_node)
+
+
+@pytest.fixture
+def named_component():
+    class NamedComponent(component_module.Component):
+        NAME = "component"
+
+    return NamedComponent
+
+
+def test_component_init(named_component):
+    mock_ipc_node = Mock()
+    mock_logger = Mock()
+    mock_ipc_node.logger = mock_logger
+    mock_redis = Mock()
+    mock_ipc_node.redis = mock_redis
+
+    component = named_component(mock_ipc_node)
+
+    assert component._state == component_module.ComponentState.STOPPED
+    assert component._ipc_node == mock_ipc_node
+    mock_ipc_node.bind_routes.assert_called_once_with(component)
+    mock_ipc_node.start.assert_called_once()
+
+    assert component.ipc_node == mock_ipc_node
+    assert component.redis == mock_redis
+    assert component.logger == mock_logger
+
+
+def test_component_get_state(named_component):
+    mock_ipc_node = Mock()
+    mock_redis = Mock()
+    mock_redis.get = Mock(return_value=component_module.ComponentState.STOPPED.encode())
+
+    component = named_component(mock_ipc_node)
+
+    assert component.get_state(mock_redis, component.NAME) == component_module.ComponentState.STOPPED
+    mock_redis.get.assert_called_once_with(f"state:component")
+
+
+# -- State update --
+def test_component_update_state(named_component):
+    mock_ipc_node = Mock()
+    mock_redis = Mock()
+    mock_ipc_node.redis = mock_redis
+    mock_logger = Mock()
+    mock_ipc_node.logger = mock_logger
+
+    component = named_component(mock_ipc_node)
+    component._state = component_module.ComponentState.STOPPED
+
+    for state in [
+        {"to": component_module.ComponentState.STARTING, "from": component_module.ComponentState.STOPPED},
+        {"to": component_module.ComponentState.STARTED, "from": component_module.ComponentState.STARTING},
+        {"to": component_module.ComponentState.STOPPING, "from": component_module.ComponentState.STARTED},
+        {"to": component_module.ComponentState.STOPPED, "from": component_module.ComponentState.STOPPING},
+    ]:
+        component._update_state(state["to"], state["from"])
+
+        assert component._state == state["to"]
+        mock_ipc_node.send.assert_called_once_with(f"state:component:{state['to']}", {"component": "component"})
+        mock_ipc_node.redis.set.assert_called_once_with("state:component", state['to'])
+        mock_ipc_node.logger.info.assert_called_once_with(f"component is {state['to']}", "component", "state")
+        mock_ipc_node.logger.reset_mock()
+        mock_ipc_node.redis.reset_mock()
+        mock_ipc_node.send.reset_mock()
+
+
+def test_component_update_state_wrong_state(named_component):
+    mock_ipc_node = Mock()
+    component = named_component(mock_ipc_node)
+    component._state = component_module.ComponentState.STOPPED
+
+    for state in [
+        {"to": component_module.ComponentState.STARTING, "from": component_module.ComponentState.STOPPED},
+        {"to": component_module.ComponentState.STARTED, "from": component_module.ComponentState.STARTING},
+        {"to": component_module.ComponentState.STOPPING, "from": component_module.ComponentState.STARTED},
+        {"to": component_module.ComponentState.STOPPED, "from": component_module.ComponentState.STOPPING},
+    ]:
+        wrong_states = [
+            component_module.ComponentState.STOPPED,
+            component_module.ComponentState.STARTING,
+            component_module.ComponentState.STARTED,
+            component_module.ComponentState.STOPPING,
+        ]
+        wrong_states.remove(state["from"])
+
+        for wrong_state in wrong_states:
+            component._state = wrong_state
+            with pytest.raises(AssertionError):
+                component._update_state(state["to"], state["from"])
+            assert component._state == wrong_state
+
+
+def test_component_set_starting(named_component):
+    mock_ipc_node = Mock()
+
+    component = named_component(mock_ipc_node)
+    component._state = component_module.ComponentState.STOPPED
+
+    with unittest.mock.patch.object(component, "_update_state") as mock_update_state:
+        component._set_starting()
+
+        mock_update_state.assert_called_once_with(
+            component_module.ComponentState.STARTING, component_module.ComponentState.STOPPED
+        )
+
+
+def test_component_set_started(named_component):
+    mock_ipc_node = Mock()
+    component = named_component(mock_ipc_node)
+    component._state = component_module.ComponentState.STARTING
+
+    with unittest.mock.patch.object(component, "_update_state") as mock_update_state:
+        component._set_started()
+
+        mock_update_state.assert_called_once_with(
+            component_module.ComponentState.STARTED, component_module.ComponentState.STARTING
+        )
+
+
+def test_component_set_stopping(named_component):
+    mock_ipc_node = Mock()
+    component = named_component(mock_ipc_node)
+    component._state = component_module.ComponentState.STARTED
+
+    with unittest.mock.patch.object(component, "_update_state") as mock_update_state:
+        component._set_stopping()
+
+        mock_update_state.assert_called_once_with(
+            component_module.ComponentState.STOPPING, component_module.ComponentState.STARTED
+        )
+
+
+def test_component_set_stopped(named_component):
+    mock_ipc_node = Mock()
+    component = named_component(mock_ipc_node)
+    component._state = component_module.ComponentState.STOPPING
+
+    with unittest.mock.patch.object(component, "_update_state") as mock_update_state:
+        component._set_stopped()
+
+        mock_update_state.assert_called_once_with(
+            component_module.ComponentState.STOPPED, component_module.ComponentState.STOPPING
+        )
+        mock_ipc_node.stop.assert_called_once()
+
+
+def test_component_has_stop_and_start(named_component):
+    mock_ipc_node = Mock()
+    component = named_component(mock_ipc_node)
+
+    assert hasattr(component, "start")
+    assert hasattr(component, "stop")
+
+    with pytest.raises(NotImplementedError):
+        component.start()
+
+    with pytest.raises(NotImplementedError):
+        component.stop()
+
+
+def test_start_and_stop_component(named_component):
+    mock_ipc_node = Mock()
+    component = named_component(mock_ipc_node)
+
+    with unittest.mock.patch.object(component, "_set_starting") as mock_set_starting:
+        with unittest.mock.patch.object(component, "_set_started") as mock_set_started:
+            with unittest.mock.patch.object(component, "_set_stopping") as mock_set_stopping:
+                with unittest.mock.patch.object(component, "_set_stopped") as mock_set_stopped:
+                    with unittest.mock.patch.object(component, "start") as mock_start:
+                        with unittest.mock.patch.object(component, "stop") as mock_stop:
+                            component.start_component()
+
+                            mock_set_starting.assert_called_once()
+                            mock_start.assert_called_once()
+                            mock_set_started.assert_called_once()
+
+                            with pytest.raises(RuntimeError):
+                                component._stop_component(component, None, None)
+
+
+# --- Integration ---
+def test_component_integration():
+    class TestComponent(component_module.Component):
+        NAME = "component"
+
+        def __init__(self, ipc_node: ipc.IpcNode):
+            super().__init__(ipc_node)
+
+            self._alive = False
+            self._flag = False
+
+        def _run(self):
+            self._flag = True
+
+            while self._alive:
+                pass
+
+            self._flag = True
+
+        def start(self):
+            self._alive = True
+            threading.Thread(target=self._run).start()
+
+        def stop(self):
+            self._alive = False
+
+    r = redis.StrictRedis(host="redis", port=6379, db=0)
+    node = ipc.IpcNode(
+        "node",
+        r,
+        r.pubsub()
+    )
+    node.set_logger(logger.Logger(node))
+
+    component = TestComponent(node)
+    component.start_component()
+    while not component._flag:
+        pass
+    component._flag = False
+    node.send("state:component:stop", {"component": "component"}, loopback=True)
+    while not component._flag:
+        pass
