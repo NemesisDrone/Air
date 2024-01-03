6f07d64289cf15e1fcb007d9a6f10badb623fc93
x <sh4ft.me@gmail.com>
2024-01-03 22:54:22 +0100

    feat: reworked sim7600 component
    Reworked the component to work with the component and ipc reworks, increased stability.
    Component is now able to be fully emulated.

--- src/sim7600/sim7600.py
+++ src/sim7600/sim7600.py
@@ -1,46 +1,87 @@
+import datetime
 import json
 import random
+import threading
+import typing
 
 import serial
 from utilities import component as component, ipc
 import time
 
+GNSS_POLL_SLEEP_TIME = 0.05
+
 
 class Sim7600:
-    def __init__(self):
+
+    GPS_TOGGLE_TIMEOUT = 2
+
+    @staticmethod
+    def _serial_setup():
+        """
+        Setup serial interface for sim7600H
+        """
         try:
-            self.ser = serial.Serial("/dev/ttyS0", 115200)
+            ser = serial.Serial("/dev/ttyS0", 115200, timeout=2)
         except Exception as e:
             # For RPI 2
-            self.ser = serial.Serial("/dev/ttyAMA0", 115200)
+            try:
+                ser = serial.Serial("/dev/ttyAMA0", 115200, timeout=2)
+            except Exception as e:
+                raise RuntimeError(f"Could not open serial interface ttyS0 or ttyAMA0: {e}")
 
-        self.ser.flushInput()
-        self.ser.flushOutput()
+        ser.flushInput()
+        ser.flushOutput()
+        return ser
 
-    def _send_at(self, command):
-        self.ser.write((command + "\r\n").encode())
+    def __init__(self):
+        """
+        Initialize sim7600H
+        """
+        self._ser = self._serial_setup()
+
+    def _send_command(self, command):
+        self._ser.write((command + "\r\n").encode())
+
+    def _flush_output(self, timeout: float = 1.0):
+        now = time.time()
         buff = ""
         while "OK" not in buff and "ERROR" not in buff:
-            buff += self.ser.readline().decode()
+            if time.time() - now > timeout:
+                raise RuntimeError(f"Unable to flush output, timeout, sim7600H may not be recognized: {buff}")
+            buff += self._ser.readline().decode()
         return buff
 
     def toggle_gps(self, enable: bool = True):
-        self._send_at("AT+CGPS=0")
-        self._send_at("AT+CGPSHOR=1")
-        self._send_at("AT+CGPSNMEARATE=1")
+        self._send_command("AT+CGPS=0")
+        self._flush_output()
+        self._send_command("AT+CGPSHOR=1")
+        self._flush_output()
+        self._send_command("AT+CGPSNMEARATE=1")
+        self._flush_output()
         if enable:
-            r = self._send_at("AT+CGPS=1,2")
+            self._send_command("AT+CGPS=1,2")
+            r = self._flush_output()
             while "OK" not in r:
                 time.sleep(0.25)
-                r = self._send_at("AT+CGPS=1,2")
+                self._send_command("AT+CGPS=1,2")
+                r = self._flush_output()
 
-    def get_gnss_info(self):
-        r = self._send_at("AT+CGNSSINFO")
+    def get_gnss_info(self) -> typing.Union[typing.Dict[str, typing.Union[int, float, str, tuple[int, float]]], None]:
+        self._send_command("AT+CGNSSINFO")
+        r = self._flush_output()
+
         try:
             if "ERROR" in r:
-                raise Exception("Could not get GPS info")
+                raise RuntimeError(f"Unable to get GNSS info: {r}")
+
             r = r.split("+CGNSSINFO: ")[1].split("\r\n")[0]
             r = r.split(",")
+
+            # Check raw data
+            for i in range(len(r)):
+                if i != 12 and r[i] == "":
+                    return None
+
             labels = [
                 "fixMode",
                 "gpsSat",
@@ -59,109 +100,122 @@ class Sim7600:
                 "hdop",
                 "vdop",
             ]
-            r = {labels[i]: r[i] for i in range(len(r))}
+            str_labels = ["latInd", "lonInd", "date", "time", "course", "lat", "lon"]
 
-            r["lat"] = (int(r["lat"][:2]), float(r["lat"][2:]))
-            r["lon"] = (int(r["lon"][:3]), float(r["lon"][3:]))
+            data: typing.Dict[str, typing.Union[int, float, str, tuple[int, float]]] = \
+                {labels[i]: float(r[i]) if labels[i] not in str_labels else r[i] for i in range(len(r))}
+
+            data["lat"] = (int(data["lat"][:2]), float(data["lat"][2:]))
+            data["lon"] = (int(data["lon"][:3]), float(data["lon"][3:]))
+            data["timestamp"] = time.time()
 
             # Data validation
-            if r["fixMode"] != "2":
-                raise Exception()
+            if data["fixMode"] != 2:
+                raise RuntimeError(f"Invalid GNSS data: {r}\n\tData: {data}")
 
         except Exception as e:
-            return False  # Not valid
-        return r
+            raise RuntimeError(f"Unable to get GNSS info: {e}\n\tData: {r}")
+
+        return data
 
 
 class Sim7600Component(component.Component):
     NAME = "sim7600"
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, ipc_node: ipc.IpcNode):
+        super().__init__(ipc_node)
 
-        self.alive = False
-        self.valid = True
+        #: Is the gnss worker alive
+        self._gnss_worker_alive = False
+        #: The gnss worker thread
+        self._gnss_worker_thread = threading.Thread(target=self._gnss_worker, daemon=True)
+        #: Is the gnss worker data valid or is it emulated data
+        self._gnss_emulation = False
 
         try:
-            self.sim = Sim7600()
-            self.sim.toggle_gps()
+            self._sim = Sim7600()
+            self._sim.toggle_gps()
         except Exception as e:
-            self.log(f"Could not initialize sim7600: {e}", level=ipc.LogLevels.WARNING)
-            self.valid = False
+            self.logger.warning(f"Could not initialize SIM7600, defaulting to emulated data: {e}", self.NAME)
+            self._gnss_emulation = True
 
-        self.r.set(
-            "state:sim7600:custom",
-            json.dumps({"valid": self.valid, "alive": self.alive}),
+        self._update_custom_status()
+
+    def _update_custom_status(self):
+        """
+        Update the custom status to sensors:sim7600:status
+        """
+        self.redis.set(
+            "sensors:sim7600:status",
+            json.dumps({"gnss_worker_alive": self._gnss_worker_alive, "gnss_emulation": self._gnss_emulation}),
         )
-        self.send("state:sim7600:custom", {"valid": self.valid, "alive": self.alive})
-
-        self.log("GPS component initialized")
-
-    def start(self):
-        self.alive = True
-
-        self.log("GPS component started")
-        return self
-
-    def do_work(self):
-        self.r.set(
-            "state:sim7600:custom",
-            json.dumps({"valid": self.valid, "alive": self.alive}),
+        self.ipc_node.send(
+            "sensors:sim7600:status",
+            {"gnss_worker_alive": self._gnss_worker_alive, "gnss_emulation": self._gnss_emulation}
         )
-        self.send("state:sim7600:custom", {"valid": self.valid, "alive": self.alive})
+
+    def _update_gnss_data(self):
+        """
+        Update the GNSS data to sensors:sim7600:gnss
+
+        :raises RuntimeError: If the GNSS data is unavailable
+        """
+        assert not self._gnss_emulation
+        data = self._sim.get_gnss_info()
+        if isinstance(data, dict):
+            self.ipc_node.send("sensors:sim7600:gnss", data)
+            self.redis.set("sensors:sim7600:gnss", json.dumps(data))
+
+    def _update_emulated_gnss_data(self):
+        data = {
+            "fixMode": 2,
+            "gpsSat": 5,
+            "gloSat": 6,
+            "beiSat": 2,
+            "lat": (48, 03.843334 + random.uniform(-0.01, 0.01)),
+            "latInd": "N",
+            "lon": (0, 45.382674 + random.uniform(-0.01, 0.01)),
+            "lonInd": "W",
+            "date": datetime.datetime.now().strftime("%d%m%Y"),
+            "time": datetime.datetime.now().strftime("%H%M%S"),
+            "alt": 60.3 + random.uniform(-0.1, 0.1),
+            "speed": 0.0,
+            "course": "",
+            "pdop": 1.0,
+            "hdop": .7,
+            "vdop": .7,
+        }
+
+        self.redis.set("sensors:sim7600:gnss", json.dumps(data))
+        self.ipc_node.send("sensors:sim7600:gnss", data)
+
+    def _gnss_worker(self):
+        # Clear eventual previous data
+        self.redis.set("sensors:sim7600:gnss", "")
+
+        # Update the new alive state
+        self._update_custom_status()
 
         try:
-            while self.alive:
-                if self.valid:
-                    data = self.sim.get_gnss_info()
-                    if type(data) == dict:
-                        self.send("sensors:sim7600:gnss", data)
-                        self.r.set("sensors:sim7600:gnss", json.dumps(data))
-                    time.sleep(0.05)
+            while self._gnss_worker_alive:
+                if not self._gnss_emulation:
+                    self._update_gnss_data()
+                    time.sleep(GNSS_POLL_SLEEP_TIME)
 
                 else:
-                    data = {
-                        "fixMode": 2,
-                        "gpsSat": 0,
-                        "gloSat": 0,
-                        "beiSat": 0,
-                        "lat": (48, 03.843334 + random.uniform(-0.01, 0.01)),
-                        "latInd": "N",
-                        "lon": (0, 45.382674 + random.uniform(-0.01, 0.01)),
-                        "lonInd": "W",
-                        "date": "191123",
-                        "time": "172039",
-                        "alt": "60.3",
-                        "speed": "0.0",
-                        "course": "",
-                        "pdop": "1.0",
-                        "hdop": "0.7",
-                        "vdop": "0.7",
-                    }
-
-                    self.send("sensors:sim7600:gnss", data)
-                    self.r.set("sensors:sim7600:gnss", json.dumps(data))
-                    time.sleep(0.1)
+                    self._update_emulated_gnss_data()
+                    time.sleep(GNSS_POLL_SLEEP_TIME)
 
         except Exception as e:
-            self.log(
-                "GPS component stopped unexpectedly: " + str(e),
-                level=ipc.LogLevels.ERROR,
-            )
-            self.alive = False
-            self.valid = False
+            self.logger.error(f"GNSS worker stopped unexpectedly: {e}", self.NAME)
+            self._gnss_worker_alive = False
 
-        self.r.set(
-            "state:sim7600:custom",
-            json.dumps({"valid": self.valid, "alive": self.alive}),
-        )
-        self.send("state:sim7600:custom", {"valid": self.valid, "alive": self.alive})
+        self._update_custom_status()
+
+    def start(self):
+        self._gnss_worker_alive = True
+        self._gnss_worker_thread.start()
 
     def stop(self):
-        self.alive = False
-        self.log("GPS component stopped")
-
-
-def run():
-    compo = Sim7600Component().start()
-    compo.do_work()
+        self._gnss_worker_alive = False
+        self._gnss_worker_thread.join()
