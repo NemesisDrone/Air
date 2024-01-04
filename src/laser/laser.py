066af8c6a6e99957c2f4f4b3cd46007c9804f222
N1coc4colA <n1coc4cola@gmail.com>
2024-01-04 23:05:12 +0100

    refactor, fix: laser not sending data after restarts.
    Add states to more precisely check errors.
    Add guards for internal states.
    Add typing.
    Add docs.
    Fix restart measurements not working by putting _do_work() in separate thread, called from start().

--- src/laser/laser.py
+++ src/laser/laser.py
@@ -1,10 +1,24 @@
 import json
 
 from utilities import component as component, ipc
+from utilities.ipc import LogLevels as ll
 import time
 import board
 import busio
-import adafruit_vl53l0x
+import adafruit_vl53l0x.VL53L0X as drv
+import threading
+
+
+class LaserState(int):
+    """
+    Class representing the different states of NVSComponent.
+    """
+    I2CInitFail: int = 0
+    VL53InitFail: int = 1
+    Unknown: int = 2
+    Initialized: int = 3
+    PendingStop: int = 4
+    Alive: int = 5
 
 
 class LaserComponent(component.Component):
@@ -19,72 +33,118 @@ class LaserComponent(component.Component):
     def __init__(self):
         super().__init__()
 
-        self.valid = True
-        self.alive = False
+        self._state: int = LaserState.Unknown
+        self.i2c: busio.I2C = None
+        self.vl53: drv = None
+        self.thread: threading.Thread = None
 
         try:
             self.i2c = busio.I2C(board.SCL, board.SDA)
-            self.vl53 = adafruit_vl53l0x.VL53L0X(self.i2c)
         except Exception as e:
-            self.log(f"Could not initialize laser: {e}", level=ipc.LogLevels.WARNING)
-            self.valid = False
-
-        self.r.set(
-            "state:laser:custom", json.dumps({"valid": self.valid, "alive": self.alive})
-        )
-        self.send("state:laser:custom", {"valid": self.valid, "alive": self.alive})
-        self.log("Laser component initialized")
-
-    def start(self):
-        if self.valid:
-            self.alive = True
-
-        return self
-
-    def do_work(self):
-        """
-        The do_work method is the main method in charge of getting the laser distance and sending it on redis IPC.
-        """
-        self.r.set(
-            "state:laser:custom", json.dumps({"valid": self.valid, "alive": self.alive})
-        )
-        self.send("state:laser:custom", {"valid": self.valid, "alive": self.alive})
-
-        if not self.alive:
+            self.set_laser_state(LaserState.I2CInitFail)
+            self.log("Failed to create I2C interface: " + str(e), ll.WARNING)
             return
 
+        if self._state != LaserState.I2CInitFail:
+            try:
+                self.vl53: drv = drv(self.i2c)
+                self.set_laser_state(LaserState.Initialized)
+            except Exception as e:
+                self.set_laser_state(LaserState.VL53InitFail)
+                self.log("Failed to init VL53 Driver: " + str(e), ll.WARNING)
+                return
+
+        self.log("Initialized.")
+
+
+    def __del__(self):
+        self.stop()
+
+
+    def start(self) -> bool:
+        """
+        Function used to start the component. It will set up a thread and schedule tasks to make the measurements.
+        """
+
+        if self._state != LaserState.Initialized:
+            self.log("Impossible to start, wrongly initialized.", ll.INFO)
+            return False
+
+        self.thread = threading.Thread(target=self._do_work)
+        self.thread.start()
+
+        self.log("Started.", ll.INFO)
+        return True
+
+
+    def stop(self) -> None:
+        """
+        Stops the measurements. It will wait until all the threads are able to stop.
+        This means that the function might be blocking for an undetermined amount of time.
+        """
+        if self._state <= LaserState.PendingStop:
+            return
+
+        # Make the measurements loop stop.
+        self.set_laser_state(LaserState.PendingStop)
+        while self._state >= LaserState.PendingStop:
+            pass
+
+        # Make the thread join.
+        if self.thread:
+            self.thread.join()
+
+        self.log("Stopped.", ll.INFO)
+
+
+    def _do_work(self) -> None:
+        """
+        The _do_work method is in charge of getting the laser distance and sending it on Redis IPC.
+        """
+        self.set_laser_state(LaserState.Alive)
+
         try:
+            # Notice that we don't need to use busio.I2C.try_lock and unlock because we have only one
+            # instance running within the program.
             with self.vl53.continuous_mode():
-                while self.alive:
+                while self._state == LaserState.Alive:
                     # Get a measurement
                     laser_distance = self.vl53.range
 
-                    # Send the measurement on redis IPC and save it in redis db
+                    # Send the measurement on Redis IPC and save it in Redis DB.
                     self.send("sensors:laser:distance", laser_distance)
                     self.r.set("sensors:laser:distance", laser_distance)
 
-                    # limit the frequency of the laser distance sensor
+                    # Sleep to limit the frequency of the measurements.
                     time.sleep(0.05)
 
+            # Set back to proper state.
+            self.set_laser_state(LaserState.Initialized)
+
         except Exception as e:
-            self.log(
-                "Laser component stopped unexpectedly: " + str(e),
-                level=ipc.LogLevels.ERROR,
-            )
-            self.alive = False
-            self.valid = False
-
-        self.r.set(
-            "state:laser:custom", json.dumps({"valid": self.valid, "alive": self.alive})
-        )
-        self.send("state:laser:custom", {"valid": self.valid, "alive": self.alive})
-
-    def stop(self):
-        self.alive = False
-        self.log("Laser component stopped")
+            self.log("Stopped unexpectedly: " + str(e), ll.ERROR)
+            self.set_laser_state(LaserState.Unknown)
 
 
-def run():
-    compo = LaserComponent().start()
-    compo.log("Laser component started")
-    compo.do_work()
+    def set_laser_state(self, val: int) -> None:
+        """
+        Sets value of the internal state. Used as guard to watch unproper behaviours on value updates.
+        :param int val: Value of a LaserState.
+        """
+        if val < LaserState.Initialized:
+            self.log("Warning, state:" + str(val), ll.CRITICAL)
+        self._state = val
+        self._publish_state()
+
+
+    def _publish_state(self) -> None:
+        """
+        Publishes the internal state of the component on Redis.
+        """
+        self.send("state:laser:custom", {"state": self._state, "alive": (self.alive >= LaserState.Alive)})
+        self.r.set("state:laser:custom", json.dumps({"state": self._state, "alive": (self.alive >= LaserState.Alive)}))
+
+
+def run() -> bool:
+    compo = LaserComponent()
+    return compo.start()
