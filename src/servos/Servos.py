f5087df1ebb03721b3009924c167a10fa95566ec
x <sh4ft.me@gmail.com>
2024-01-05 10:40:26 +0100

    refacto: tmp refacto to follow ipc & component rework for sensor component

--- src/servos/Servos.py
+++ src/servos/Servos.py
@@ -1,3 +1,4 @@
+import threading
 import time
 from typing import List
 
@@ -7,7 +8,6 @@ import os
 import json
 import pigpio
 os.system("pigpiod")
-from utilities.ipc import route
 
 
 class ServosComponent(component.Component):
@@ -17,8 +17,8 @@ class ServosComponent(component.Component):
     """
     NAME = "servos"
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, ipc_node: ipc.IpcNode):
+        super().__init__(ipc_node)
 
         self.pi = pigpio.pi()
         self.nb_canals = 10
@@ -26,17 +26,16 @@ class ServosComponent(component.Component):
         self.alive = False
 
     def start(self):
-        self.log("Servos component started")
         self.alive = True
 
-        self.r.set("servos:canal:1", 180)
+        self.redis.set("servos:canal:1", 180)
 
         for i in range(1, self.nb_canals + 1):
-            angle = self.r.get(f"servos:canal:{i}")
+            angle = self.redis.get(f"servos:canal:{i}")
             if not angle:
                 continue
             # Get the gpio of the servo from the config
-            canal = json.loads(self.r.get(f"config:canal:{i}"))
+            canal = json.loads(self.redis.get(f"config:canal:{i}"))
             gpios: List[int] = canal["gpios"]
 
             if len(gpios) == 0:
@@ -44,6 +43,8 @@ class ServosComponent(component.Component):
             # Set all servos to their middle position
             for gpio in gpios:
                 self.pi.set_servo_pulsewidth(gpio, 1500)
+
+        threading.Thread(target=self.servos_work, daemon=True).start()
 
     @staticmethod
     def calculate_pulsewidth_from_angle(angle: float) -> float:
@@ -59,11 +60,11 @@ class ServosComponent(component.Component):
         This method is used to update the angles of the all servos, based on the redis values
         """
         for i in range(1, self.nb_canals + 1):
-            angle = self.r.get(f"servos:canal:{i}")
+            angle = self.redis.get(f"servos:canal:{i}")
             if not angle:
                 continue
             # Get the gpio of the servo from the config
-            canal = json.loads(self.r.get(f"config:canal:{i}"))
+            canal = json.loads(self.redis.get(f"config:canal:{i}"))
             gpios: List[int] = canal["gpios"]
 
             if len(gpios) == 0:
@@ -80,14 +81,3 @@ class ServosComponent(component.Component):
 
     def stop(self):
         self.pi.stop()
-        self.log("Servos component stopped")
-
-
-def run():
-    compo = ServosComponent()
-    compo.start()
-    compo.servos_work()
-
-
-if __name__ == "__main__":
-    run()
